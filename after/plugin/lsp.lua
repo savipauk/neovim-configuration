bb5929098109dad174e4cf69498b2cb8d6a2343e
savipauk <erik.kranjec8@outlook.com>
2025-05-14 16:54:57 +0200

    Remove gopls ensure_installed lsp list

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,7 +53,7 @@ lspconfig.tailwindcss.setup({
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'clangd', 'lua_ls', 'gopls' },
+  ensure_installed = { 'clangd', 'lua_ls' },
   handlers = {
     lsp_zero.default_setup,
   },
